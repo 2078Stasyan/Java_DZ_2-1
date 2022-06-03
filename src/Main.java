5d7db1f911a7f13763a6db7e5364dc5fd96931ae
2078Stasyan <2078lsv@gmail.com>
2022-06-03 05:23:44 +0400

    Замена переменных с маленькой буквы

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int TicketPrice = 1650; //стоимость билета
+        int ticketPrice = 1650; //стоимость билета
         int money = 20; //количество рублей для одной бонусной милли
-        int bonus = TicketPrice / money; //количество начисленных мили
+        int bonus = ticketPrice / money; //количество начисленных мили
 
         System.out.println("Количество начисленных мили за купленный билет состовляет:");
         System.out.println((bonus) + " мили");
